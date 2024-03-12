012077a898563ae2d6b361438925d80c61f92571
Vejaysundaram <vejaysundaramrajkumar@gmail.com>
2024-03-12 21:50:10 +0530

    Firebase intergration
    Have done firebase intergration so that the database is stored in cloud and can be accessed with any device

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
 from flask import Flask, render_template, request, redirect, session, url_for
-import firebase_admin
-from firebase_admin import credentials, auth, db
 from datetime import datetime
+import pyrebase
 
-cred = credentials.Certificate("D:\projects\mini_project-2024\credentials.json")
-firebase_admin.initialize_app(cred, {
-    'databaseURL': 'https://testproject1-2ff78-default-rtdb.asia-southeast1.firebasedatabase.app/'
-})
+# Firebase configuration
+config  = {
+  "apiKey": "AIzaSyCFu9G0-QWPUtGV3iONI32q4n69__jMM6E",
+  "authDomain": "test-2-18803.firebaseapp.com",
+  "databaseURL": "https://test-2-18803-default-rtdb.asia-southeast1.firebasedatabase.app",
+  "projectId": "test-2-18803",
+  "storageBucket": "test-2-18803.appspot.com",
+  "messagingSenderId": "914801447775",
+  "appId": "1:914801447775:web:fd28b9390557973f611cea"
+}
+
+firebase = pyrebase.initialize_app(config)
+auth = firebase.auth()
+data_b = firebase.database()
+
 
 app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
@@ -44,12 +54,8 @@ def signup():
             blood_group = request.form.get('bgroup')
             password = request.form.get('pword')
 
-            # Check if email already exists
-            user_ref = db.reference('users')
-            existing_user = user_ref.order_by_child('email').equal_to(email).get()
-
-            if existing_user:
-                return render_template("error.html", error_title="Please use a different one.", error_message="Email already exists!")
+            name=first_name+' '+last_name
+            
 
             # Calculate age based on the provided date of birth
             dob_date = datetime.strptime(dob, '%Y-%m-%d')
@@ -60,33 +66,32 @@ def signup():
             if age < 18 or age >= 65:
                 return render_template("error.html", error_message="Age must be between 18 and 64 years old.")
 
-            # Create a new user in Firebase Authentication
-            new_user = auth.create_user(
-                email=email,
-                password=password,
-                display_name=first_name + " " + last_name
-            )
-
-            # Store additional user details in Realtime Database
-            user_ref.child(new_user.uid).set({
-                'name': new_user.display_name,
-                'email': email,
-                'phonenumber': phone,
-                'bloodgroup': blood_group,
-                'dob': dob
-            })
+            # Create user with email and password
+            user = auth.create_user_with_email_and_password(email, password)
+            
+            # Store additional details in the Realtime Database
+            user_data = {
+                "name": name,
+                "email": email,
+                "phone": phone,
+                "age": age,
+                "blood_group": blood_group,
+                
+            }
+            data_b.child("users").child(user["localId"]).set(user_data)
+            print("User created successfully.")
 
             # Set the username in the session
-            session['username'] = new_user.display_name
+            session['username'] =name
 
-            return render_template("index.html", current_user=new_user.display_name)
+            return render_template("index.html",current_user=name)
 
         return render_template("getstarted.html")
     except Exception as e:
         return render_template("error.html", error_title="Signup Error!@#$", error_message=str(e))
 
-@app.route('/login', methods=['POST', 'GET'])
-def login():
+@app.route('/loginsubmit', methods=['POST', 'GET'])
+def loginsubmit():
     try:
         if 'username' in session:
             return redirect(url_for('index'))
@@ -94,16 +99,15 @@ def login():
         if request.method == 'POST':
             email = request.form.get('email')
             password = request.form.get('password')
-
             try:
-                # Sign in with email and password
                 user = auth.sign_in_with_email_and_password(email, password)
-                session['username'] = user['displayName']
-                return render_template("index.html", current_user=user['displayName'])
+                userid=user['localId']
+                user_name = data_b.child("users").child(userid).child('name').get().val()
+                return render_template("index.html",current_user=user_name)
             except Exception as auth_error:
+                # If authentication fails, render error template with the error message
                 return render_template("error.html", error_message=str(auth_error))
-
-        return render_template("login.html")
+    
     except Exception as e:
         return render_template("error.html", error_message="Unexpected error while logging in", error_title="Login error!")
 
@@ -111,6 +115,10 @@ def login():
 def logout():
     session.pop('username', None)
     return redirect(url_for('index'))
+
+@app.route('/login')
+def login():
+    return render_template("login.html")
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0")
