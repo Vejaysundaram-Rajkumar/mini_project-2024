044ac22540697d282e0af0dae5dc8dfcc1238d8d
Vejaysundaram <vejaysundaramrajkumar@gmail.com>
2024-05-05 20:57:15 +0530

    now calling is added to it
    When an request is made then an sms and an call is triggred so as to intimate the registred users about the requirement of the blood

--- main.py
+++ main.py
@@ -4,8 +4,16 @@ import keys
 
 client=Client(keys.acc_sid,keys.auth_token)
 message=client.messages.create(
-    body="Testing the message sending for my mini project",
+    body="Testing the message sending for my mini project and will be calling for intimation",
     from_=keys.twilio_no,
     to=keys.my_no
 )
 print(message.body)
+
+call=client.calls.create(
+    twiml='<Responce><Say>Hello this is an blood request alert from bloodline community</Say></Responce>',
+    to=keys.my_no,
+    from_=keys.twilio_no
+)
+
+print(call.sid)
